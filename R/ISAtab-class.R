263927b30325d271ed4f123ddfd176031f742ac4
Alejandra Gonzalez-Beltran <alejandra.gonzalez.beltran@gmail.com>
2012-08-29 10:10:44 +0100

    Moved class to separate file, added documentation

---
+++ R/ISAtab-class.R
@@ -0,0 +1,30 @@
+require(methods) || stop("Couldn't load package methods")
+
+### class to hold ISATab information
+#' An S4 class that stores ISAtab information.
+#' ISAtab-class
+#' 
+#' @export
+ISAtab <- setClass("ISAtab",
+                   representation(
+                     path="character",
+                     investigation.filename="character",
+                     investigation.file="data.frame",
+                     study.identifiers="factor",
+                     study.filenames="character",
+                     study.files="list",
+                     assay.filenames="character",
+                     assay.filenames.per.study="list",
+                     assay.files="list",
+                     assay.files.per.study="list",
+                     assay.technology.types="character",
+                     assay.measurement.types="character",
+                     data.filenames="list",
+                     samples="character",
+                     samples.per.study="list",
+                     samples.per.assay.filename="list",
+                     assay.filenames.per.sample="character",
+                     sample.to.rawdatafile="list",
+                     sample.to.assayname="list",
+                     rawdatafile.to.sample="list",
+                     assayname.to.sample="list"))
--- R/Risa.R
+++ R/Risa.R
@@ -25,56 +25,6 @@ technology.types <- list(
   )
 
 
-### class to hold ISATab information
-ISAtab <- setClass("ISAtab",
-         representation(
-           path="character",
-           investigation.filename="character",
-           investigation.file="data.frame",
-           study.identifiers="factor",
-           study.filenames="character",
-           study.files="list",
-           assay.filenames="character",
-           assay.filenames.per.study="list",
-           assay.files="list",
-           assay.files.per.study="list",
-           assay.technology.types="character",
-           assay.measurement.types="character",
-           data.filenames="list",
-           samples="character",
-           samples.per.study="list",
-           samples.per.assay.filename="list",
-           assay.filenames.per.sample="character",
-           sample.to.rawdatafile="list",
-           sample.to.assayname="list",
-           rawdatafile.to.sample="list",
-           assayname.to.sample="list"))
-
-## getters 
-setMethod(f="[",signature="ISAtab", definition=function(x, i,j, drop) {
-      if (i=="path") { return(x@path) } else {}
-      if (i=="investigation.filename") { return(x@investigation.filename) } else {}
-      if (i=="investigation.file") { return(x@investigation.file) } else {}
-      if (i=="study.identifiers") { return(x@study.identifiers) } else {}
-      if (i=="study.filenames") { return(x@study.filenames) } else {}
-      if (i=="study.files") { return(x@study.files) } else {}   
-      if (i=="assay.filenames") { return(x@assay.filenames) } else {}
-      if (i=="assay.filenames.per.study") { return(x@assay.filenames.per.study) } else {}
-      if (i=="assay.files") { return(x@assay.files) } else {}
-      if (i=="assay.files.per.study") { return(x@assay.files.per.study) } else {}
-      if (i=="assay.technology.types") { return(x@assay.technology.types) } else {}
-      if (i=="assay.measurement.types") { return(x@assay.measurement.types) } else {}
-      if (i=="data.filenames") { return(x@data.filenames) } else {}
-      if (i=="samples") { return(x@samples) } else {}
-      if (i=="samples.per.study") { return(x@samples.per.study) } else {}
-      if (i=="samples.per.assay.filename") { return(x@samples.per.assay.filename) } else {}
-      if (i=="assay.filenames.per.sample") { return(x@assay.filenames.per.sample) } else {}
-      if (i=="sample.to.rawdatafile") { return(x@sample.to.rawdatafile) } else {}
-      if (i=="sample.to.assayname") { return(x@sample.to.assayname) } else {}
-      if (i=="rawdatafile.to.sample") { return(x@rawdatafile.to.sample) } else {}
-      if (i=="assayname.to.sample") { return(x@assayname.to.sample) } else {}
-        }
-          ) 
 
 ## This function only works if the zip file does not contain a directory (but the ISA-TAB files themselves)
 isatab2bioczip = function(zip, path = getwd(), verbose=FALSE)
