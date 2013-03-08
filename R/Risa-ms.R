3f2f87ceb70f9b398facb55935bbdd2c8268f18c
Alejandra Gonzalez-Beltran <alejandra.gonzalez.beltran@gmail.com>
2013-03-08 14:36:00 +0000

    The method processAssayXcmsSet now uses all the factors to build the sclass object, instead of just the first one #12

--- R/Risa-ms.R
+++ R/Risa-ms.R
@@ -6,19 +6,22 @@ processAssayXcmsSet.1factor = function(isa, assay.filename, ...){
   
   i <- which(isa["assay.filenames"]==assay.filename)
   
+  #if 'Raw Spectral Data File' is one of the columns in the assay file = it is a mass spectrometry assay
   if (isatab.syntax$raw.spectral.data.file %in% colnames(isa["data.filenames"][[i]]))
   {
     #mass spectrometry files
     msfiles = isa["data.filenames"][[i]][[ isatab.syntax$raw.spectral.data.file ]]
     
+    #the assay file as an AnnotatedDataFrame
     pd = try(read.AnnotatedDataFrame(file.path(isa["path"], isa["assay.filenames"][i]),
                                      row.names = NULL, blank.lines.skip = TRUE, fill = TRUE,
                                      varMetadata.char = "$", quote="\""))
     
+    #Adding the raw spectral data files as the row names
     sampleNames(pd) = pd$Raw.Spectral.Data.File
     
     if (length(grep(isatab.syntax$factor.value, colnames(isa["assay.files"][[i]]))) != 0) {
-      ## If there are explicit factors, use them
+      ## If there are explicit factors, use the first of them
       sclass = isa["assay.files"][[i]][ which(isa["assay.files"][[i]][[isatab.syntax$sample.name]] %in% pd$Sample.Name), grep(isatab.syntax$factor.value, colnames(isa["assay.files"][[i]]))[1]]
       
       wd <- getwd()
@@ -41,23 +44,16 @@ processAssayXcmsSet.1factor = function(isa, assay.filename, ...){
 
 
 ### specific function to deal with assays whose technology type is mass spectrometry using the xcms package
-### it returns an xcmsSet
-### TODO - change implementation to include all factors
+### it returns an xcmsSet, 
 processAssayXcmsSet = function(isa, assay.filename, ...){
-  
-  
-  phenodata.data.frame <- as.data.frame(isa["factors"])
-  
-  assay.names <- isa["assay.names"][assay.filename]
-  
-  row.names(phenodata.data.frame) <- assay.names[[assay.filename]][[1]]
-  
+      
   i <- which(isa["assay.filenames"]==assay.filename)
   
   if (isatab.syntax$raw.spectral.data.file %in% colnames(isa["data.filenames"][[i]]))
-  {
+  {    
+              
     #mass spectrometry files
-    msfiles = isa["data.filenames"][[i]][[ isatab.syntax$raw.spectral.data.file ]]
+    msfiles = isa["data.filenames"][[i]][[ isatab.syntax$raw.spectral.data.file ]]        
     
     pd = try(read.AnnotatedDataFrame(file.path(isa["path"], isa["assay.filenames"][i]),
                                      row.names = NULL, blank.lines.skip = TRUE, fill = TRUE,
@@ -67,11 +63,12 @@ processAssayXcmsSet = function(isa, assay.filename, ...){
     
     if (length(grep(isatab.syntax$factor.value, colnames(isa["assay.files"][[i]]))) != 0) {
       ## If there are explicit factors, use them
-      sclass = isa["assay.files"][[i]][ which(isa["assay.files"][[i]][[isatab.syntax$sample.name]] %in% pd$Sample.Name), grep(isatab.syntax$factor.value, colnames(isa["assay.files"][[i]]))[1]]
+      sclass <- as.data.frame(isa["factors"])
       
       wd <- getwd()
       setwd(isa["path"])
       xset = xcmsSet(files=msfiles, sclass=sclass, ...)
+          
       setwd(wd)
       
     } else {
